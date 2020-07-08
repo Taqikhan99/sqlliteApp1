e84d13228371d72f0104c842b0a404f7ac3b5c92
taqi khan <taqiahmed339@gmail.com>
2020-07-08 21:21:05 +0500

    Hello World app

--- app/src/main/java/com/example/jokesapp/MainActivity.java
+++ app/src/main/java/com/example/jokesapp/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.jokesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1,btn2,btn3,btn4;
+    TextView ans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1=findViewById(R.id.btn_1);
+        btn2=findViewById(R.id.btn_2);
+        btn3=findViewById(R.id.btn_3);
+        btn4=findViewById(R.id.btn_4);
+        ans=findViewById(R.id.textAns);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ans.setText("Usko dusri side pe jana tha!!");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ans.setText("Wo red paint he!");
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ans.setText("Me table hun!");
+            }
+        });
+
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ans.setText("Me Watermelon hun!");
+            }
+        });
     }
 }
